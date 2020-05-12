3ebed1e4dcbca0edd67086c583a9eefa334bac1c
liuzhenxin <956201564@qq.com>
2020-05-12 15:34:36 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
   <div id="app">
+    <div class="header">
+      <div class="header-bar">123</div>
+      <div class="header-filter"></div>
+    </div>
     <router-view />
   </div>
 </template>
@@ -10,4 +14,20 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+  .header{
+    position: fixed;
+    top:0;
+    left: 0;
+    right: 0;
+
+    background: #f2f2f2;
+  }
+  .header .header-bar{
+    height:2.333333rem /* 87.5/37.5 */;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+</style>
