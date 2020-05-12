865622eed98c4976e8dbebd0ec5d14b8fd11aed9
liuzhenxin <956201564@qq.com>
2020-05-12 15:06:03 +0800

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<template>
+  <div></div>
+</template>
+<script>
+export default {
+  name: "",
+  data() {
+    return {};
+  },
+  methods: {},
+  mounted() {}
+};
+</script>
+<style lang="" scoped></style>
---
+++ src/views/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+      <div>hello word</div>
+  </div>
+</template>
+<script>
+export default {
+  name: "",
+  data() {
+    return {};
+  },
+  methods: {},
+  mounted() {}
+};
+</script>
+<style lang="" scoped></style>
